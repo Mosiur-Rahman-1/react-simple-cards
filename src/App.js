c5e2bb6b4b0de6c54d16f397bf417aa982c7c02e
Mosiur-Rahman-1 <masiurreheman01@gmail.com>
2020-08-21 21:30:53 +0600

    react simple card design copied form adobe site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from '../icons/';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "main-container">
+      <p className = "heading">Most used adobe apps</p>
+      <hr className= "heading-line"/>
+      <Products name = "Photoshop" price = "US$20.99/mo" 
+        description = "Lightroom, Photoshop, Lightroom Classic, and 20GB of cloud storage (1TB available)."
+        source = {process.env.PUBLIC_URL + "/icons/photoshop.png"}></Products>
+      <Products name = "Illustrator" price = "US$20.99/mo" 
+        description = "Lightroom, Photoshop, Lightroom Classic, and 20GB of cloud storage (1TB available)."
+        source = {process.env.PUBLIC_URL + "/icons/illustrator.png"}></Products>
+      <Products name = "InDesign" price = "US$19.99/mo" 
+        description = "Lightroom, Photoshop, Lightroom Classic, and 20GB of cloud storage (1TB available)."
+        source = {process.env.PUBLIC_URL + "/icons/indesign.png"}></Products>
+      <Products name = "After Effects" price = "US$9.99/mo" 
+        description = "Lightroom, Photoshop, Lightroom Classic, and 20GB of cloud storage (1TB available)."
+        source = {process.env.PUBLIC_URL + "/icons/after-effects.png"}></Products>
     </div>
+    
   );
+}
+
+function Products(props) {
+return (
+
+  <div className ="secondary-container">
+    <div className ="first-child">
+      <img className= "child-img" src={props.source} alt=""/>
+      <h3>{props.name}</h3>
+      <h2>{props.price}</h2>
+      <p>{props.description}</p>
+      <button className = "child-btn">Buy now</button>
+    </div>
+  </div>
+);
 }
 
 export default App;
